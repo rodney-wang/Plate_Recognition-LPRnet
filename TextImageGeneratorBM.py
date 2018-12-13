13ad052625f40ee541eeecc6359e39e98f137a19
rodney-wang <wfei@aibee.com>
2018-12-13 02:01:51 -0800

    test benchmark accuracy

---
+++ TextImageGeneratorBM.py
@@ -0,0 +1,170 @@
+import os
+import numpy as np
+import cv2
+import json
+
+CHARS = ['京', '沪', '津', '渝', '冀', '晋', '蒙', '辽', '吉', '黑',
+         '苏', '浙', '皖', '闽', '赣', '鲁', '豫', '鄂', '湘', '粤',
+         '桂', '琼', '川', '贵', '云', '藏', '陕', '甘', '青', '宁',
+         '新',
+         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K',
+         'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+         'W', 'X', 'Y', 'Z','_'
+         ]
+dict = {'A01':'京','A02':'津','A03':'沪','B02':'蒙',
+        'S01':'皖','S02':'闽','S03':'粤','S04':'甘',
+        'S05': '贵', 'S06': '鄂', 'S07': '冀', 'S08': '黑', 'S09': '湘',
+        'S10': '豫', 'S12': '吉', 'S13': '苏', 'S14': '赣', 'S15': '辽',
+        'S17': '川', 'S18': '鲁', 'S22': '浙',
+        'S30':'渝', 'S31':'晋', 'S32':'桂', 'S33':'琼', 'S34':'云', 'S35':'藏',
+        'S36':'陕','S37':'青', 'S38':'宁', 'S39':'新'}
+
+CHARS_DICT = {char:i for i, char in enumerate(CHARS)}
+
+NUM_CHARS = len(CHARS)
+
+
+class TextImageGeneratorBM:
+    def __init__(self, img_dir, label_file, batch_size, img_size, num_channels=3, label_len=7):
+        self._img_dir = img_dir
+        self._label_file = label_file
+        self._batch_size = batch_size
+        self._num_channels = num_channels
+        self._label_len = label_len
+        self._img_w, self._img_h = img_size
+
+        self._num_examples = 0
+        self._next_index = 0
+        self._num_epoches = 0
+        self.filenames = []
+        self.labels = []
+
+        self.init()
+
+    def init(self):
+        self.labels = []
+        fs = os.listdir(self._img_dir)
+        plate_label = json.load(open(self._label_file))
+
+        for filename in fs:
+            if filename[-4:] == '.png':
+                bname = filename.split('_plate.png')[0]
+                chars = plate_label[bname]
+                #if '\u4e00' <= label[0] <= '\u9fff':
+                #    label = filename[:7]
+                if len(chars) >=7:
+                    self.filenames.append(filename)
+                    label = encode_label(chars[:7])
+                    print(label)
+                    self.labels.append(label)
+                    self._num_examples += 1
+        self.labels = np.float32(self.labels)
+
+    def next_batch(self):
+        # Shuffle the data
+        if self._next_index == 0:
+            perm = np.arange(self._num_examples)
+            np.random.shuffle(perm)
+            self._filenames = [self.filenames[i] for i in perm]
+            self._labels = self.labels[perm]
+
+        batch_size = self._batch_size
+        start = self._next_index
+        end = self._next_index + batch_size
+        if end > self._num_examples:
+            self._next_index = 0
+            start = self._next_index
+            end = self._next_index + batch_size
+            self._num_epoches += 1
+        else:
+            self._next_index = end
+        images = np.zeros([batch_size, self._img_h, self._img_w, self._num_channels])
+        # labels = np.zeros([batch_size, self._label_len])
+
+        for j, i in enumerate(range(start, end)):
+            fname = self._filenames[i]
+            img = cv2.imread(os.path.join(self._img_dir, fname))
+            img = cv2.resize(img, (self._img_w, self._img_h), interpolation=cv2.INTER_CUBIC)
+            images[j, ...] = img
+        images = np.transpose(images, axes=[0, 2, 1, 3])
+        labels = self._labels[start:end, ...]
+        targets = [np.asarray(i) for i in labels]
+        sparse_labels = sparse_tuple_from(targets)
+        # input_length = np.zeros([batch_size, 1])
+
+        seq_len = np.ones(self._batch_size) * 24
+        return images, sparse_labels, seq_len
+
+def sparse_tuple_from(sequences, dtype=np.int32):
+    """
+    Create a sparse representention of x.
+    Args:
+        sequences: a list of lists of type dtype where each element is a sequence
+    Returns:
+        A tuple with (indices, values, shape)
+    """
+    indices = []
+    values = []
+
+    for n, seq in enumerate(sequences):
+        indices.extend(zip([n] * len(seq), range(len(seq))))
+        values.extend(seq)
+
+    indices = np.asarray(indices, dtype=np.int64)
+    values = np.asarray(values, dtype=dtype)
+    shape = np.asarray([len(sequences), np.asarray(indices).max(0)[1] + 1], dtype=np.int64)
+
+    return indices, values, shape
+
+
+def decode_sparse_tensor(sparse_tensor):
+    decoded_indexes = list()
+    current_i = 0
+    current_seq = []
+    for offset, i_and_index in enumerate(sparse_tensor[0]):
+        i = i_and_index[0]
+        if i != current_i:
+            decoded_indexes.append(current_seq)
+            current_i = i
+            current_seq = list()
+        current_seq.append(offset)
+    decoded_indexes.append(current_seq)
+    result = []
+    for index in decoded_indexes:
+        result.append(decode_a_seq(index, sparse_tensor))
+    return result
+
+
+def decode_a_seq(indexes, spars_tensor):
+    decoded = []
+    for m in indexes:
+        str = CHARS[spars_tensor[1][m]]
+        decoded.append(str)
+    return decoded
+
+def encode_label(s):
+    label = np.zeros([len(s)])
+    for i, c in enumerate(s):
+        label[i] = CHARS_DICT[c]
+    return label
+
+
+
+def report_accuracy(decoded_list, test_targets):
+    original_list = decode_sparse_tensor(test_targets)
+    detected_list = decode_sparse_tensor(decoded_list)
+    true_numer = 0
+
+    if len(original_list) != len(detected_list):
+        print("len(original_list)", len(original_list), "len(detected_list)", len(detected_list),
+              " test and detect length desn't match")
+        return
+    print("T/F: original(length) <-------> detectcted(length)")
+    for idx, number in enumerate(original_list):
+        detect_number = detected_list[idx]
+        hit = (number == detect_number)
+        #print(hit, number, "(", len(number), ") <-------> ", detect_number, "(", len(detect_number), ")")
+        if hit:
+            true_numer = true_numer + 1
+    print("Test Accuracy:", true_numer * 1.0 / len(original_list))
---
+++ batch_benchmark.py
@@ -0,0 +1,85 @@
+#coding=utf-8
+import os
+import argparse
+import numpy as np
+import sys
+import cv2
+import glob
+import tensorflow as tf
+import time
+from LPRtf3 import get_train_model
+from TextImageGeneratorBM import TextImageGeneratorBM, report_accuracy
+
+#/ssd/zq/parkinglot_pipeline/carplate/test_data/image_data
+
+BATCH_SIZE = 256
+img_size = [94, 24]
+num_channels = 3
+label_len = 7
+
+
+
+def batch_test(img_dir, label_file):
+
+    logits, inputs, targets, seq_len = get_train_model(num_channels, label_len, BATCH_SIZE, img_size)
+    logits = tf.transpose(logits, (1, 0, 2))
+    # tragets是一个稀疏矩阵
+    decoded, log_prob = tf.nn.ctc_beam_search_decoder(logits, seq_len, merge_repeated=False)
+
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as session:
+        session.run(init)
+        saver = tf.train.Saver(tf.global_variables(), max_to_keep=100)
+        saver.restore(session, './mode/LPRtf3.ckpt-42000')
+        test_gen = TextImageGeneratorBM(img_dir=img_dir,
+                                      label_file=label_file,
+                                      batch_size=BATCH_SIZE,
+                                      img_size=img_size,
+                                      num_channels=num_channels,
+                                      label_len=label_len)
+        #do_report(test_gen, 3)
+        for i in range(4):
+            test_inputs, test_targets, test_seq_len = test_gen.next_batch()
+            test_feed = {inputs: test_inputs,
+                         targets: test_targets,
+                         seq_len: test_seq_len}
+            st = time.time()
+            dd = session.run(decoded[0], test_feed)
+            tim = time.time() - st
+            print('time:%s' % tim)
+            report_accuracy(dd, test_targets)
+
+
+
+def run_lpr(img_dir, out_dir):
+    input_imgs = glob.glob(img_dir + '/*.jpg')
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    for i, image_name in enumerate(input_imgs):
+        print(i, image_name)
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Plate end to end test')
+    parser.add_argument('--img_dir', default='/ssd/wfei/data/testing_data/wanda_plates_v1.2',
+                        type=str, help='Input test image dir')
+    parser.add_argument('--out_dir', default='/ssd/wfei/data/testing_data/lpr_results_v1.0',
+                        type=str, help='Output image dir')
+    parser.add_argument('--label_file', default='/ssd/wfei/data/testing_data/wanda_benchmark_label.json',
+                        type=str, help='Output image dir')
+
+    args = parser.parse_args()
+    return args
+
+
+
+if __name__ == '__main__':
+
+    args = parse_args()
+
+    #run_lpr(args.img_dir, args.out_dir)
+    batch_test(args.img_dir, args.label_file)
+
