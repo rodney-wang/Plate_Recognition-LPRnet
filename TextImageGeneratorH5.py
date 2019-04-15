8c24d2c1ae104dbe06b3040a047dd820488dc7e8
wfei <wfei@gpu028.aibee.cn>
2019-04-16 07:41:23 +0800

    get rid of print

--- TextImageGeneratorH5.py
+++ TextImageGeneratorH5.py
@@ -87,25 +87,22 @@ class TextImageGeneratorH5:
         end = self._next_index + batch_size
         if end > self._num_examples:
             self._next_index = 0
-            #start = self._next_index
-            #end = self._next_index + batch_size
             start = self._num_examples - batch_size
             end = self._num_examples
             self._num_epoches += 1
         else:
             self._next_index = end
 
-        #labels = np.zeros([batch_size, self._label_len])
         images = np.zeros([batch_size, self._img_h, self._img_w, self._num_channels])
         for i,j in enumerate(range(start, end)):
             img = np.squeeze(self.X[j, ...])
             img = augment_data(img)
             img = cv2.resize(img, (self._img_w, self._img_h), interpolation=cv2.INTER_CUBIC)
             images[i, ...] = img[..., np.newaxis]
-        print "Batch image shape:", images.shape
         images = np.transpose(images, axes=[0, 2, 1, 3])
         labels = self.labels[start:end, ...]
-        print labels
+        #print "Batch image shape:", images.shape
+        #print "Batch label shape:", labels.shape
         targets = [np.asarray(i) for i in labels]
         sparse_labels = sparse_tuple_from(targets)
         # input_length = np.zeros([batch_size, 1])
@@ -119,9 +116,11 @@ if __name__ == '__main__':
     img_size = [94, 24]
 
     train_gen = TextImageGeneratorH5(h5_path=h5_path,
-                                   batch_size=32,
+                                   batch_size=256,
                                    img_size=img_size,
                                    num_channels=1,
                                    label_len=8)
-    images, sparse_labels, seq_len = train_gen.next_batch()
-    print sparse_labels, seq_len
+    for i in range(100):
+       print "Batch i"
+       images, sparse_labels, seq_len = train_gen.next_batch()
+       #print sparse_labels, seq_len
