ac20393e0fb8b08195ea78b0a3f67253583fcde3
rodney-wang <wfei@aibee.com>
2019-04-19 14:09:03 -0700

    evaluate energy cars

---
+++ eval_lpr.py
@@ -0,0 +1,64 @@
+import os
+import glob
+import time
+import argparse
+from eval_generic import eval
+from plate_ocr_lpr import PlateOCR
+
+#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+"""
+ Process all plates in benchmark
+"""
+
+def run_lpr_and_write_result(plate_file, out_dir, pocr):
+    plate_file = plate_file.strip()
+
+    if not os.path.exists(plate_file.encode('utf-8')):
+        print('File does not exist')
+        return
+    chars, score = pocr(plate_file)
+
+    if score >=0 and len(chars) != 0:
+        fname = os.path.basename(plate_file).split('_plate')[0]
+        fname = fname.replace('.jpg', '.txt')
+
+        out_file = os.path.join(out_dir, fname)
+
+        out_str = ' '.join([chars, str(score)])
+        print fname, out_str
+        # with open(out_file, 'w', encoding='utf-8') as ff:
+        with open(out_file, 'w') as ff:
+            #ff.write(out_str.encode('utf-8'))
+            ff.write(out_str)
+    return True
+
+
+def lpr_eval_end2end(ocrtxt_file, out_dir, model_path, skip):
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    pocr = PlateOCR(model_path)
+    start_time = time.time()
+    for line in open(ocrtxt_file, 'r'):
+        plate_file, label = line.split(';')
+        run_lpr_and_write_result(plate_file, out_dir, pocr)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    eval(ocrtxt_file, out_dir, skip)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Plate Segmentation')
+    parser.add_argument('--ocr_txt', default='/ssd/wfei/data/plate_for_label/energy_cars/energy_plates_ocrlabel_test_217.txt',
+                        type=str, help='Input test image dir')
+    parser.add_argument('--out_dir', default='/ssd/wfei/data/ocr_results/energy_plates_res',
+                        type=str, help='Output image dir')
+    parser.add_argument('--model', default='/ssd/wfei/code/Plate_Recognition-LPRnet/model_pb_h5', type=str, help='Caffe model path')
+    parser.add_argument('--skip', default=0, type=int, help='Skip any characters in evaluation')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    lpr_eval_end2end(args.ocr_txt, args.out_dir, args.model, args.skip)
---
+++ plate_ocr_lpr.py
@@ -0,0 +1,52 @@
+#coding=utf-8
+import cv2
+import os
+import numpy as np
+from tensorflow.contrib import predictor
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+class PlateOCR:
+
+    def __init__(self, model_dir='/ssd/wfei/code/Plate_Recognition-LPRnet/model_pb_h5', num_channel=1):
+        self.predict_fn = predictor.from_saved_model(model_dir)
+        self.num_channel = num_channel
+
+    def __call__(self, img):
+        """ Plate OCR with LPR
+         results: chars, score
+        """
+        plate_file = img.strip()
+
+        if not os.path.exists(plate_file.encode('utf-8')):
+            print('File does not exist')
+            return
+        img = cv2.imread(plate_file)
+        img = cv2.resize(img, (94, 24))
+        if self.num_channel == 1:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            images = img[np.newaxis, :, :, np.newaxis]
+        else:
+            images = img[np.newaxis, :]
+
+        images = np.transpose(images, axes=[0, 2, 1, 3])
+
+        inputs = {"Placeholder": images,
+                  "Placeholder_4": [24]}
+        # "is_train": False}
+
+        predictions = self.predict_fn(inputs)
+        print "Prediction:      ", predictions['code2str_conversion/predicted'][0].decode('utf-8')
+        print "Confidence Score:", predictions['confidence_score'][0]
+        chars = predictions['code2str_conversion/predicted'][0].decode('utf-8')
+        score = predictions['confidence_score'][0]
+
+        return chars, score
+
+if __name__ == '__main__':
+
+    test_img = '/mnt/soulfs2/wfei/data/plate.jpg'
+    pocr = PlateOCR()
+    chars, score = pocr(test_img)
+    print chars, score
+
+
