ffab0592e1cfc5939af4837486a34b97586c2440
wfei <wfei@gpu028.aibee.cn>
2019-06-05 07:33:32 +0800

    refine on wanda data

--- LPRtf3_c1_h5.py
+++ LPRtf3_c1_h5.py
@@ -11,7 +11,7 @@ from TextImageGeneratorH5 import TextImageGeneratorH5, sparse_tuple_from
 
 from config import CHARS, dict, CHARS_DICT, NUM_CHARS
 
-os.environ["CUDA_VISIBLE_DEVICES"]="4,5"
+os.environ["CUDA_VISIBLE_DEVICES"]="2,3"
 
 #训练最大轮次
 num_epochs = 100
@@ -27,11 +27,11 @@ REPORT_STEPS = 4000
 
 #训练集的数量
 BATCH_SIZE = 256
-TRAIN_SIZE = 234437 
+TRAIN_SIZE = 161000 
 BATCHES = TRAIN_SIZE//BATCH_SIZE
 test_num = 3
 
-ti = '/ssd/wfei/code/Plate_Recognition-LPRnet/data/wanda_train'         #训练集位置
+ti = '/ssd/wfei/code/Plate_Recognition-LPRnet/data/wanda_train_0604'         #训练集位置
 vi = '/ssd/wfei/code/Plate_Recognition-LPRnet/data/lpr_test'         #验证集位置
 img_size = [94, 24]
 tl = None
@@ -196,7 +196,7 @@ def train(a):
         #print(b_cost, steps)
         if steps > 0 and steps % REPORT_STEPS == 0:
             do_report(val_gen,test_num)
-            saver.save(session, "./model_wanda/LPR_wanda.ckpt", global_step=steps)
+            saver.save(session, "./model_wanda_0604/LPR_wanda.ckpt", global_step=steps)
         return b_cost, steps
 
     with tf.Session() as session:
@@ -204,7 +204,7 @@ def train(a):
         saver = tf.train.Saver(tf.global_variables(), max_to_keep=100)
         if a=='train':
              start_epoch = 0
-             checkpoint = './model_h5/LPR_grayh5.ckpt-60000'
+             checkpoint = './model_wanda/LPR_wanda.ckpt-80000'
              saver.restore(session, checkpoint)
              checkpoint_id = 0
              start_epoch = checkpoint_id // BATCHES
